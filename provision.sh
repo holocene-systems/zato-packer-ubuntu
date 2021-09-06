6f95c44f91d0f79bdcb2013128518e1f4a7a4cde
samrose <samuel.rose@gmail.com>
2021-09-06 16:22:54 -0400

    zato-packer-ubuntu

---
+++ provision.sh
@@ -0,0 +1,32 @@
+sudo apt-get update -y && sudo apt-get install -y \
+    apt-transport-https \
+    curl \
+    git \
+    htop \
+    libcurl4-openssl-dev \
+    redis-server \
+    software-properties-common \
+    ssh \
+    wget \
+    sudo \
+    supervisor \
+    vim \
+    libpq-dev \
+    gnupg \
+    whois \
+    libsasl2-dev \
+    libldap2-dev \
+    libssl-dev
+
+
+curl -s https://zato.io/repo/zato-3.2-48849AAD40BCBB0E.pgp.txt | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://zato.io/repo/stable/3.2/ubuntu $(lsb_release -cs) main"
+
+sudo apt-get update
+
+sudo apt-get install -y zato 
+
+#cd /opt/zato/current  &&  git pull && ./update.sh
+
+sudo su - zato -c "zato --version"
